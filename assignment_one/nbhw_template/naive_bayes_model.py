1cd18730b0cc9c8ca3d5f37685cdfd78ff2102d1
Eric Bronner <erbronne@gmail.com>
2015-03-05 00:42:54 -0500

    folders

---
+++ assignment_one/nbhw_template/naive_bayes_model.py
@@ -0,0 +1,106 @@
+import random, collections
+
+class NB(object) :
+    def __init__(self, prior, likelihoods):
+        """make a naive Bayes model with a specified prior over classes
+        and class-dependent likelihood distributions"""
+        self.prior = prior
+        self.likelihoods = likelihoods
+        self.featuredict = {}
+    
+    def most_informative_features(self, n=100):
+        features = set()
+        maxprob = collections.defaultdict(lambda: 0.0)
+        minprob = collections.defaultdict(lambda: 1.0)
+        for cat in self.prior.outcomes():
+            for fdist in self.likelihoods[cat]:
+                for fval in fdist.outcomes():
+                    feature = (fdist.variable, fval)
+                    features.add( feature )
+                    p = fdist.probability(fval)
+                    maxprob[feature] = max(p, maxprob[feature])
+                    minprob[feature] = min(p, minprob[feature])
+                    if minprob[feature] == 0:
+                        features.discard(feature)
+
+        # Convert features to a list, & sort it by how informative
+        # features are.
+        features = sorted(features, key=lambda feature_: minprob[feature_]/maxprob[feature_])
+        return features[:n]
+
+        
+    def _cat_prob(self, event, category):
+        """compute the probability of the event in this model, 
+        assuming that the event actually belongs to the specified category"""
+        p = self.prior.probability(category)
+        for dist in self.likelihoods[category]:
+            if dist.variable in event:
+                p = p * dist.probability(event[dist.variable])
+        return p
+    
+    def probability(self, event):
+        """compute the probability of an event according to the model, 
+        marginalizing over the event category if it's not specified"""
+        if self.prior.variable in event:
+            category = event[self.prior.variable]
+            return self._cat_prob(event, category)
+        else:
+            p = 0.
+            for category in self.prior.outcomes():
+                p = p + self._cat_prob(event, category)
+            return p
+           
+    def classify(self, event):
+        """classify an event according to its probability in the model"""
+        return max(self.prior.outcomes(), 
+                   key = lambda c: self._cat_prob(event, c))
+    
+    def outcomes(self):
+        """creates a generator that yields all the (dictionary, probability) pairs
+        where dictionary gives a specific event specified by the model 
+        and probability gives the probability the model assigns to this event"""
+        def last(desc, p):
+            return (dict(desc), p)
+        def extend(generator, dist):
+            for desc, p in generator:
+                for o in dist.outcomes():
+                    yield ([(dist.variable, o)] + desc, p * dist.probability(o))
+        for c in self.prior.outcomes():
+            gen = (([(self.prior.variable, o)], self.prior.probability(o)) for o in [c])
+            for dist in self.likelihoods[c]:
+                gen = extend(gen, dist)
+            for desc, p in gen:
+                yield last(desc, p)
+    
+    def accuracy(self, rule):
+        """compute the expected accuracy of a specific classification rule
+        according to the model.  Rule is a function from events to classes.
+        Applies rule to each of the outcomes() of the model, with the true
+        category of the outcome hidden, and sums the probability that
+        the rule predicts the correct class for that outcome"""
+        def correct(event):
+            copy = dict(event)
+            del copy[self.prior.variable]
+            prediction = rule(copy)
+            return prediction == event[self.prior.variable]
+        return sum(p for (event, p) in self.outcomes() if correct(event))
+    
+    def optimum(self):
+        """return the performance of the best possible classifier
+        (namely, the one that uses the true probabilities to make its decision)"""
+        return self.accuracy(self.classify)
+    
+    def sample(self):
+        """create a random event using the distribution defined by the model"""
+        event = {}
+        (k, v) = self.prior.sample()
+        event[k] = v
+        for dict in self.likelihoods[v]:
+            (k, v) = dict.sample()
+            event[k] = v
+        return event
+    
+    def __repr__(self) :
+        specs = lambda v:''.join([('  %s\n' % dist) for dist in self.likelihoods[v]])
+        classes = [('Given %s\n%s' % (v, specs(v))) for v in self.prior.table]
+        return "Naive Bayes Model\n%s\n%s" % (self.prior, ''.join(classes))          
---
+++ assignment_one/nbhw_template/problem_two.py
@@ -0,0 +1,90 @@
+from naive_bayes_model import NB
+from distribution import Distribution
+from observations import *
+from test import frequent_events_data
+import random, collections
+
+class id_node:
+	def __init__(self, identifier):
+		self.identifier = identifier
+		self.values = dict() #dict of {value:count}
+		self.children = dict() #dict of {value:{feature:{subvalue:count}}}
+		self.total_count = 0
+
+def NB_from_data(prediction, data):
+	#given frequent_events_data, create the NB object that goes into, say frequent_events_example
+	head = count_data(data)
+	head = probabilitize_data(head)
+	
+	prior = Distribution(head.identifier, head.values)
+
+	likelihood = dict()
+	for prior_value in head.children:
+		likelihood[prior_value] = list()
+		for feat in head.children[prior_value]:
+			variable = feat
+			table = head.children[prior_value][feat]
+			likelihood[prior_value].append(Distribution(variable, table))
+
+	bayes = NB(prior, likelihood) 		
+	
+	return bayes
+
+def probabilitize_data(node):
+	sum = 0
+	for key, value in node.values.items():
+		sum += value
+	for key in node.values:
+		node.values[key] = node.values[key] / (sum * 1.0)
+
+	for value, feat_dict in node.children.items():
+		for feat, subval_dict in node.children[value].items():
+			sum = 0
+			for subvalue, count in node.children[value][feat].items():
+				sum += count
+			for subvalue in node.children[value][feat]:
+				node.children[value][feat][subvalue] = node.children[value][feat][subvalue] / (sum * 1.0)
+
+	#print node.identifier
+	#print node.values
+	#print node.children
+	
+	return node
+
+def count_data(data):
+	head = init_data(data)
+	for datum in data:
+		first_key = head.identifier
+		first_value = None
+		for key, value in datum.items():
+			if key == first_key:
+				head.values[value] += 1
+				first_value = value
+			else:
+				head.children[first_value][key][value] += 1
+	
+	#print head.identifier
+	#print head.values
+	#print head.children
+	
+	return head			
+
+def init_data(data):
+	"""creates a data structure to parse and count the data"""
+	feats = list(get_features(data))
+	head = id_node(feats[0])
+	for value in get_values(feats[0], data):
+		head.values[value] = 0
+		head.children[value] = dict()
+		for i in range(1, len(feats)):
+			head.children[value][feats[i]] = dict()
+			for subvalue in get_values(feats[i], data):
+				head.children[value][feats[i]][subvalue] = 0
+	
+	#print head.identifier
+	#print head.values
+	#print head.children
+	
+	return head
+
+#print NB_from_data(None, frequent_events_data)
